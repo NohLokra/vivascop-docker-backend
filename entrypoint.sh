c9a72dfe9ac4437049fd8b85ccfbcdd495d50a5c
thibaut severac <thib3113@gmail.com>
2018-04-22 11:55:30 +0200

    init

---
+++ entrypoint.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# usage: file_env VAR [DEFAULT]
+#    ie: file_env 'XYZ_DB_PASSWORD' 'example'
+# (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
+#  "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
+file_env(){
+	local var="$1"
+	local fileVar="${var}_FILE"
+	local def="${2:-}"
+	if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+		echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
+		exit 1
+	fi
+	local val="$def"
+	if [ "${!var:-}" ]; then
+		val="${!var}"
+	elif [ "${!fileVar:-}" ]; then
+		val="$(< "${!fileVar}")"
+	fi
+	export "$var"="$val"
+	unset "$fileVar"
+}
+
+file_env 'SSH_KEY'
+file_env 'GIT_REPO'
+
+#add SSH_KEY
+mkdir -p /root/.ssh
+chmod 0700 /root/.ssh
+ssh-keyscan github.com > /root/.ssh/known_hosts
+echo \"${SSH_KEY}\" > /root/.ssh/id_rsa
+chmod 600 /root/.ssh/id_rsa
+eval `ssh-agent -s`
+ssh-add $HOME/.ssh/id_rsa
+
+#clone the repo
+git clone $GIT_REPO app
+cd app
+git git submodule update --init --recursive --remote --merge
+
+#install node app
+npm i
+
+#start app
+npm start
